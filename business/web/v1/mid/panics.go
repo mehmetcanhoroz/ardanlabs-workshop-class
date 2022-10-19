7704bffcd19fe7b07ee5b39d039c71794a152b9e
William Kennedy <bill@ardanlabs.com>
2022-10-19 10:44:30 -0400

    day3 push8

--- business/web/v1/mid/panics.go
+++ business/web/v1/mid/panics.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"runtime/debug"
 
+	"github.com/ardanlabs/service/business/web/metrics"
 	"github.com/ardanlabs/service/foundation/web"
 )
 
@@ -23,6 +24,9 @@ func Panics() web.Middleware {
 					// Stack trace will be provided.
 					trace := debug.Stack()
 					err = fmt.Errorf("PANIC [%v] TRACE[%s]", rec, string(trace))
+
+					// Updates the metrics stored in the context.
+					metrics.AddPanics(ctx)
 				}
 			}()
 
